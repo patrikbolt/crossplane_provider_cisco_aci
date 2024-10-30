657db31b02dd02667231d8228f9dd9188ad3bba1
Patrik Bolt <patrik.bolt@gmail.com>
2024-10-30 12:33:52 +0000

    fixed types

--- apis/v1alpha1/providerconfig_types.go
+++ apis/v1alpha1/providerconfig_types.go
@@ -1,44 +1,45 @@
 package v1alpha1
 
 import (
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ProviderConfigSpec specifies the Configuration for the ACI Provider
+// ProviderConfigSpec specifies the configuration for the ACI Provider.
 type ProviderConfigSpec struct {
-	// URL of the Cisco ACI API
-	URL string `json:"url"`
+    // URL of the Cisco ACI API.
+    URL string `json:"url"`
 
-	// CredentialsSecretRef refers to the Kubernetes Secret, where
-	// the Login Credentials  (Username and Password) for the Cisco ACI API is stored
-	CredentialsSecretRef xpv1.SecretKeySelector `json:"credentialsSecretRef"`
+    // CredentialsSecretRef refers to the Kubernetes Secret containing
+    // the credentials (username and password) for the Cisco ACI API.
+    CredentialsSecretRef xpv1.SecretKeySelector `json:"credentialsSecretRef"`
 
-	// InsecureSkipVerify when set to true skips the SSL Certificate Verification
-	InsecureSkipVerify bool `json:"insecureSkipVerify"`
+    // InsecureSkipVerify skips SSL certificate verification when set to true.
+    InsecureSkipVerify bool `json:"insecureSkipVerify"`
 }
 
-// ProviderConfigStatus represents the Status of ProviderConfig
+// ProviderConfigStatus represents the status of the ProviderConfig.
 type ProviderConfigStatus struct {
-	xpv1.ConditionedStatus `json:",inline"`
+    xpv1.ResourceStatus `json:",inline"`
 }
 
 // +kubebuilder:object:root=true
 
-// ProviderConfig configures the Connection to the Cisco ACI API
+// ProviderConfig configures a connection to the Cisco ACI API.
 type ProviderConfig struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ProviderConfigSpec   `json:"spec"`
-	Status ProviderConfigStatus `json:"status,omitempty"`
+    Spec   ProviderConfigSpec   `json:"spec"`
+    Status ProviderConfigStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
-// ProviderConfigList contains a List of ProviderConfig Objects
+// ProviderConfigList contains a list of ProviderConfig objects.
 type ProviderConfigList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []ProviderConfig `json:"items"`
+    metav1.TypeMeta `json:",inline"`
+    metav1.ListMeta `json:"metadata,omitempty"`
+    Items           []ProviderConfig `json:"items"`
 }
+
--- apis/v1alpha1/tenant_epg_types.go
+++ apis/v1alpha1/tenant_epg_types.go
@@ -1,46 +1,47 @@
 package v1alpha1
 
 import (
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// TenantEPGSpec defines the wished State of TenantEPG
+// TenantEPGSpec defines the desired state of TenantEPG.
 type TenantEPGSpec struct {
-	xpv1.ResourceSpec `json:",inline"`
-	ForProvider       TenantEPGParameters `json:"forProvider"`
+    xpv1.ResourceSpec `json:",inline"`
+    ForProvider       TenantEPGParameters `json:"forProvider"`
 }
 
-// TenantEPGParameters are the configurable Fields of a TenantEPG
+// TenantEPGParameters are the configurable fields of TenantEPG.
 type TenantEPGParameters struct {
-	Name       string `json:"name"`
-	Tenant     string `json:"tenant"`
-	AppProfile string `json:"appProfile"`
-	Desc       string `json:"desc"`
-	Bd         string `json:"bd"`
+    Name       string `json:"name"`
+    Tenant     string `json:"tenant"`
+    AppProfile string `json:"appProfile"`
+    Desc       string `json:"desc"`
+    Bd         string `json:"bd"`
 }
 
-// TenantEPGStatus defines the observed State of a TenantEPG
+// TenantEPGStatus defines the observed state of TenantEPG.
 type TenantEPGStatus struct {
-	xpv1.ResourceStatus `json:",inline"`
+    xpv1.ResourceStatus `json:",inline"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
-// TenantEPG is the Schema for the TenantEPG API
+// TenantEPG is the Schema for the TenantEPG API.
 type TenantEPG struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              TenantEPGSpec   `json:"spec"`
-	Status            TenantEPGStatus `json:"status,omitempty"`
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
+    Spec              TenantEPGSpec   `json:"spec"`
+    Status            TenantEPGStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
-// TenantEPGList contains a List of TenantEPGs
+// TenantEPGList contains a list of TenantEPGs.
 type TenantEPGList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []TenantEPG `json:"items"`
+    metav1.TypeMeta `json:",inline"`
+    metav1.ListMeta `json:"metadata,omitempty"`
+    Items           []TenantEPG `json:"items"`
 }
+
